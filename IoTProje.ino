324de1a66097bfa52d4ea2dbbd31ff04cc230ac8
Enes Eminler <enes.eminler@gmail.com>
2018-11-01 01:45:49 +0300

    Birleştirilmiş

--- IoTProje.ino
+++ IoTProje.ino
@@ -1,26 +1,33 @@
-const int trig = 10;
-const int echo = 8;
-const int echo2 = 9;
+const int trig = 11;
+const int echo = 10;
+const int trig2 = 13;
+const int echo2 = 12;
+
+const int led = 9;
+const int led2 = 8;
 
 const int sol_i = 2;
 const int sol_g = 3;
 const int sag_i = 4;
 const int sag_g = 5;
 
-int sure = 0;
-int mesafe = 0;
-int sure2 = 0;
-int mesafe2 = 0;
+long sure = 0;
+long mesafe = 0;
+long sure2 = 0;
+long mesafe2 = 0;
 
 void setup() {
   // put your setup code here, to run once:
  pinMode(trig, OUTPUT);
+ pinMode(trig2, OUTPUT);
  pinMode(echo, INPUT);
  pinMode(echo2, INPUT);
  pinMode(sol_i, OUTPUT);
  pinMode(sol_g, OUTPUT);
  pinMode(sag_i, OUTPUT);
  pinMode(sag_g, OUTPUT);
+ pinMode(led, OUTPUT);
+ pinMode(led2, OUTPUT);
  Serial.begin(9600);
 }
 
@@ -28,47 +35,79 @@ void loop() {
   // put your main code here, to run repeatedly:
 
  digitalWrite(trig, HIGH);
- delayMicroseconds(1000);
+ delayMicroseconds(30);
  digitalWrite(trig ,LOW);
-
+ 
  sure = pulseIn(echo , HIGH);
  mesafe = (sure/2) / 28.5;
  Serial.println (mesafe);
-
-  sure2 = pulseIn(echo2 , HIGH);
+ 
+ delayMicroseconds(30);
+ 
+ digitalWrite(trig2, HIGH);
+ delayMicroseconds(30);
+ digitalWrite(trig2,LOW);
+ 
+ sure2 = pulseIn(echo2 , HIGH);
  mesafe2 = (sure2/2) / 28.5;
  Serial.println (mesafe2);
 
  
-if ( mesafe < 30 && mesafe2 > 30)
+if (mesafe < 30 && mesafe2 < 30)
 {
-  digitalWrite(sol_i ,HIGH);
+    
+  delay(100);
+  digitalWrite(led ,HIGH);
+  delay(50);
+  digitalWrite(led2 ,HIGH);
+  delay(250);
+  digitalWrite(led ,LOW);
+  delay(50);
+  digitalWrite(led2 ,LOW);
+
+  digitalWrite(sol_i ,LOW);
   digitalWrite(sol_g ,LOW);
   digitalWrite(sag_i ,LOW);
   digitalWrite(sag_g ,LOW);
-  //Sol Led yak
 }
 else if(mesafe > 30 && mesafe2 < 30)
 {
+  digitalWrite(led2 ,HIGH);
+  delay(250);
+  digitalWrite(led2 ,LOW);
+
  digitalWrite(sol_i , LOW);
- digitalWrite(sol_g , LOW);
+ digitalWrite(sol_g , HIGH);
  digitalWrite(sag_i , HIGH);
+ digitalWrite(sol_g , LOW);
  digitalWrite(sag_g , LOW);
-  //Sağ Led yak
+ digitalWrite(sag_i , LOW);
+ 
+ 
 }
-else if(mesafe < 30 && mesafe2 < 30)
+else if(mesafe < 30 && mesafe2 > 30)
 {  
- digitalWrite(sol_i , LOW);
+ digitalWrite(led ,HIGH);
+ delay(250);
+ digitalWrite(led ,LOW);
+ 
+ digitalWrite(sol_i , HIGH);
  digitalWrite(sol_g , LOW);
  digitalWrite(sag_i , LOW);
+ digitalWrite(sag_g , HIGH);
  digitalWrite(sag_g , LOW);
-  //Çift Led yak
+ digitalWrite(sol_i , LOW);
+ 
+
 }
 else{
  digitalWrite(sol_i , HIGH);
  digitalWrite(sol_g , LOW);
  digitalWrite(sag_i , HIGH);
  digitalWrite(sag_g , LOW);
+  digitalWrite(led ,LOW);
+  digitalWrite(led2 ,LOW);
+  
 }
 //İleri geri kodları
 //Her harekette 3. led yak
